71c63db032aff7a80915f36dca815c6f2c7af785
Peak <pglund18@yahoo.com>
2023-05-19 18:22:49 -0500

    html,css,java start, form and header

---
+++ script.js
@@ -0,0 +1,19 @@
+var Quest = documant.querySelector(".Questions")
+var A1 = documant.querySelector("#A1")
+var A2 = documant.querySelector("#A2")
+var A3 = documant.querySelector("#A3")
+var A4 = documant.querySelector("#A4")
+
+
+
+//GIVEN I am taking a code quiz
+//WHEN I click the start button
+//THEN a timer starts and I am presented with a question
+//WHEN I answer a question
+//THEN I am presented with another question
+//WHEN I answer a question incorrectly
+//THEN time is subtracted from the clock
+//WHEN all questions are answered or the timer reaches 0
+//THEN the game is over
+//WHEN the game is over
+//THEN I can save my initials and my score

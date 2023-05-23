6e0ebafea903e257b785cf85dbf51c28a3779d76
Peak <pglund18@yahoo.com>
2023-05-23 16:57:35 -0500

    Forms now replace each other and all forms made

--- script.js
+++ script.js
@@ -7,19 +7,27 @@ var A4 = document.querySelector("#A4")
 
 var countdownTime = 75;
 var timerElement =document.getElementById("time")
+
 var StartBtn = document.getElementById('StartBtn');
+StartBtn.addEventListener('click', showForm2)
 
-var A1 = document.getElementById('A1');
-A1.addEventListener('click', showForm3)
+var ButtonA1 = document.getElementById('2A1');
+ButtonA1.addEventListener('click', showForm3)
 
-var A2 = document.getElementById('3A3');
-A2.addEventListener('click', showForm4)
+var ButtonA2 = document.getElementById('3A3');
+ButtonA2.addEventListener('click', showForm4)
 
-var A3 = document.getElementById('4A4');
-A3.addEventListener('click', showForm5)
+var ButtonA3 = document.getElementById('4A4');
+ButtonA3.addEventListener('click', showForm5)
 
-var A4 = document.getElementById('5A1');
-A4.addEventListener('click', showForm6)
+var ButtonA4 = document.getElementById('5A1');
+ButtonA4.addEventListener('click', showForm6)
+
+var ButtonA5 = document.getElementById('6A2');
+ButtonA5.addEventListener('click', showForm7)
+
+var Submit = document.getElementById('SubmitBtn');
+Submit.addEventListener('click', showForm8)
 
 
 StartBtn.addEventListener('click', function(event) {
@@ -38,7 +46,7 @@ function startCountdown() {
 
     if (remainingTime <= 0) {
       clearInterval(timerInterval);
-      timerElement.textContent = 'Countdown finished!';
+      timerElement.textContent = 'Failed';
     } else {
       var seconds = remainingTime;
 
@@ -48,32 +56,46 @@ function startCountdown() {
 }
 
 
+
 function showForm2(event) {
     document.getElementById('form2').style.display = 'block';
-    document.getElementById('form3').style.display = 'none';
+    document.getElementById('Form1').style.display = 'none';
     event.preventDefault();
   }
 
   function showForm3(event) {
     document.getElementById('form3').style.display = 'block';
-    document.getElementById('form4').style.display = 'none';
+    document.getElementById('form2').style.display = 'none';
     event.preventDefault();
   }
 
   function showForm4(event) {
     document.getElementById('form4').style.display = 'block';
-    document.getElementById('form5').style.display = 'none';
+    document.getElementById('form3').style.display = 'none';
     event.preventDefault();
   }
 
   function showForm5(event) {
     document.getElementById('form5').style.display = 'block';
-    document.getElementById('form6').style.display = 'none';
+    document.getElementById('form4').style.display = 'none';
     event.preventDefault();
   }
 
   function showForm6(event) {
     document.getElementById('form6').style.display = 'block';
+    document.getElementById('form5').style.display = 'none';
+    event.preventDefault();
+  }
+
+  function showForm7(event) {
+    document.getElementById('form7').style.display = 'block';
+    document.getElementById('form6').style.display = 'none';
+    event.preventDefault();
+  }
+
+  function showForm8(event) {
+    document.getElementById('form8').style.display = 'block';
+    document.getElementById('form7').style.display = 'none';
     event.preventDefault();
   }
 
